2ccb7f122a6dd6f87619470f4b8bccb1fe2778c3
Librarian Night <AndreySergeevich2004@mail.ru>
2022-04-10 19:00:34 +0300

    Update CharacterMovement.cs

--- Assets/Scripts/CharacterMovement.cs
+++ Assets/Scripts/CharacterMovement.cs
@@ -6,18 +6,17 @@ public class CharacterMovement : MonoBehaviour
 {
     public float speed = 6.0f;
     public float gravity = -9.81f;
-    [SerializeField] GameObject player;
+    private Rigidbody2D rigidbody2D;
     // Start is called before the first frame update
     void Start()
     {
+        rigidbody2D = GetComponent<Rigidbody2D>();
     }
-
+    
     // Update is called once per frame
     void Update()
     {
-        float deltaX = Input.GetAxis("Horizontal") * speed;
-        float deltaZ = Input.GetAxis("Vertical") * speed;
-        
-
+        float move = Input.GetAxis("Horizontal");
+        rigidbody2D.velocity = new Vector2(move * speed, 0);
     }
 }
